7f059e8a6d33985cd422db2e3904db5ad7f37f30
Oliver Crossley <ollythecrossley@outlook.com>
2023-10-20 21:18:47 +0100

    adds a test secret function

--- controllers/api-controllers.js
+++ controllers/api-controllers.js
@@ -1,10 +1,25 @@
 const fs = require("fs/promises");
 
-exports.getEndpoints = (require, response, next) => {
+exports.getEndpoints = (request, response, next) => {
     return fs.readFile('data/endpoints.json', "utf-8")
         .then(fileRead => {
             return JSON.parse(fileRead)
         }).then((apis) => {
             response.status(200).send(apis)
         }).catch(err => next(err))
+}
+
+exports.getSecret = (request, response, next) => {
+    return fs.readFile('data/private.json', "utf-8")
+        .then(fileRead => {
+            console.log(fileRead)
+            return JSON.parse(fileRead)
+        }).then((secret) => {
+            console.log("Accessed!")
+            response.status(200).send(secret)
+        }).catch(err => {
+            console.log("There was an error!")
+            console.log(err)
+            next(err)
+        })
 }
