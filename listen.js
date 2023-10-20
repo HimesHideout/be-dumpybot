853360ed96ec798419673b54f564aaf85737f126
Oliver Crossley <ollythecrossley@outlook.com>
2023-10-20 17:31:08 +0100

    adds future .env switching for dev and prod

--- listen.js
+++ listen.js
@@ -1,5 +1,9 @@
 const app = require("./app");
 
+const ENV = "development";
+const pathToCorrectFile = `${__dirname}/.env.${ENV}`;
+require("dotenv").config({ path: pathToCorrectFile });
+
 const start = async () => {
     try {
         app.listen(9090, () => console.log("Server started on port 9090"));
