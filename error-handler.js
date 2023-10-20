817f42615b630a6b39182992d0f40148a7df5a2a
Oliver Crossley <ollythecrossley@outlook.com>
2023-10-20 17:25:03 +0100

    adds error handler for api errors

---
+++ error-handler.js
@@ -0,0 +1,10 @@
+exports.handleServerError = (err, req, res, next) => {
+    console.log(err);
+    res.status(500).send({ msg: "Internal server error" });
+};
+
+exports.handle404s = (error, request, response, next) => {
+    if (error.status === 404) {
+        response.status(error.status).send({ msg: error.msg });
+    }
+};
