d37fcd1c74f7fbe06c36c6bb3cb2add977de6b66
Oliver Crossley <ollythecrossley@outlook.com>
2023-10-30 12:39:29 +0000

    adds test for api/players/:userId

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,7 @@ const app = require("../app");
 
 const ENV = process.env.NODE_ENV;
 const pathToCorrectFile = `${__dirname}/../.env.${ENV}`;
-require("dotenv").config({ path: pathToCorrectFile });
+require("dotenv").config({path: pathToCorrectFile});
 
 describe("/api", () => {
     test("GET 200   | Returns 200 and an object with correct endpoint values within", () => {
@@ -60,3 +60,46 @@ describe("/api/players", () => {
             })
     });
 })
+
+describe('/api/players/:userId', () => {
+    test('GET 200   | Returns 200 and an object with the correct endpoint values on it', () => {
+        return request(app)
+            .get("/api/players/168129756255092737")
+            .expect(200)
+            .then(({body}) => {
+                const player = body.player
+                expect(Object.keys(player).length > 0).toBe(true);
+                expect(player).toHaveProperty("balance");
+                expect(player).toHaveProperty("userId");
+                expect(player).toHaveProperty("inventory");
+            })
+    });
+    test('GET 200   | Returns 200 and a player object with the correct userId', () => {
+        return request(app)
+            .get("/api/players/168129756255092737")
+            .expect(200)
+            .then(({body}) => {
+                const player = body.player
+                expect(player).toHaveProperty("userId");
+                expect(player.userId).toBe("168129756255092737")
+            })
+    });
+    test('GET 400   | Returns 400 when passed an invalid userId format (contains letters) with appropriate error message', () => {
+        return request(app)
+            .get("/api/players/myuserId")
+            .expect(400)
+            .then(({body}) => {
+                expect(body).toHaveProperty("msg");
+                expect(body.msg).toBe("Invalid userId format")
+            })
+    });
+    test('GET 404   | Returns 404 when passed a valid but nonexistent userId', () => {
+        return request(app)
+            .get("/api/players/18364892302783782")
+            .expect(404)
+            .then(({body}) => {
+                expect(body).toHaveProperty("msg");
+                expect(body.msg).toBe("Player not found")
+            })
+    });
+});
