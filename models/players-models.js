85aff7e5273ff0381d1dc6edcab102aa2ff60fe3
Oliver Crossley <ollythecrossley@outlook.com>
2023-10-30 00:15:05 +0000

    creates models for players

---
+++ models/players-models.js
@@ -0,0 +1,20 @@
+// const {ScanCommand} = require("@aws-sdk/lib-dynamodb")
+/*    const command = new ScanCommand({
+        TableName: "test-players",
+    });*/
+
+const {ddbDocClientFull} = require("../database/connection");
+
+exports.selectPlayers = () => {
+
+    const scanParams = {
+        TableName: "test-players"
+    }
+
+    return ddbDocClientFull
+        .scan(scanParams)
+        .then((data) => {
+            console.log(data.Items)
+            return data.Items
+        })
+}
