b46b3ac50e517d9aaf1fe7dc8f9a0448c1e01da4
Oliver Crossley <ollythecrossley@outlook.com>
2023-10-30 20:39:12 +0000

    adds PATCH api/players/userId

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { auth } = require('express-oauth2-jwt-bearer');
 
 const {handleErrors} = require("./error-handler");
 const {getEndpoints, getSecret} = require("./controllers/api-controllers");
-const {getPlayers, getPlayerById} = require("./controllers/players-controllers");
+const {getPlayers, getPlayerById, patchPlayerById} = require("./controllers/players-controllers");
 
 const app = express();
 
@@ -28,6 +28,10 @@ app.get("/api/secret", jwtCheck, getSecret);
 app.get("/api/players", getPlayers)
 
 app.get("/api/players/:userId", getPlayerById)
+
+// noinspection JSCheckFunctionSignatures
+app.patch("/api/players/:userId", jwtCheck , patchPlayerById)
+
 // Error Handling
 app.use((req, res) => {
     res.status(404).send({ msg: "url not found" });
