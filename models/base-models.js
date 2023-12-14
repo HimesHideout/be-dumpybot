14956c168b79e8c1c12d0456d1cce9d791ea3f42
Oliver Crossley <ollythecrossley@outlook.com>
2023-12-14 15:42:09 +0000

    creates selectAll and selectById base models

---
+++ models/base-models.js
@@ -0,0 +1,32 @@
+const {ddbDocClientFull} = require("../database/connection");
+
+exports.selectAll = (table) => {
+
+    const scanParams = {
+        TableName: table
+    }
+
+    return ddbDocClientFull
+        .scan(scanParams)
+        .then((data) => {
+            return data.Items
+        })
+}
+
+exports.selectById = (table, key, value) => {
+    const getParams = {
+        TableName: table,
+        Key: {
+            [key]: value
+        }
+    }
+    return ddbDocClientFull
+        .get(getParams)
+        .then((data) => {
+            if (data.Item === undefined) {
+                return Promise.reject({status: 404, msg: `${value} not found`});
+            } else {
+                return data.Item
+            }
+        })
+}
